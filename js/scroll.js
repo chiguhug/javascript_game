9c3f05a0b07416a46a559ed6a78f55e1a922bfee
chiguhug <chiguhug@osaka-human.net>
2024-05-07 10:26:50 +0900

    コメントの追加

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var ul = document.getElementById('scroll_content');
-    var li = ul.getElementsByTagName('li');
-    for (var i=0; i < li.length; i++) {
+    //DOMの読み込み完了後に実行
+    var ul = document.getElementById('scroll_content'); //スクロール対象となるulタグの取得
+    var li = ul.getElementsByTagName('li'); //ulタグ内の全liタグの取得
+    for (var i=0; i < li.length; i++) { //liタグの数だけ繰り返す
+        //liタグがクリックされた時の処理
         li[i].addEventListener('click', function() {
-            // this.remove();
-            var input = this.getElementsByTagName('input');
-            var frame = document.getElementById('frame');
-            frame.src = input[0].value;
+            var input = this.getElementsByTagName('input'); //inputタグの取得
+            var frame = document.getElementById('frame');   //iframeタグの取得
+            frame.src = input[0].value; //inputタグの値（ゲームアプリのURL）をiframeのsrc属性にセット
         });
     };
 });
